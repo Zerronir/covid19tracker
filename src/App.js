767809054b33d4e877003d311ee430d72967f6cb
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-10-11 17:12:18 +0200

    Comienzo del proyecto del tutorial de react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {MenuItem, Button} from '@material-ui/core';
+import './App.css';
+
+function App() {
+    return ( 
+    <div className = "app">
+        <h1> Hello World! </h1>
+
+        <MenuItem></MenuItem>
+
+      {/*HEADER*/}
+      {/*Title + select country*/}
+      
+      {/*Info box*/}
+      {/*Info box*/}
+      {/*Info box*/}
+
+      {/*Table*/}
+      {/*Graph*/}
+
+      {/*Map*/}
+    </div>
+    );
+}
+
+export default App;
