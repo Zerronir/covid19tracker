b66843c3649b91652faa3761f59e214ec4575901
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-10-11 17:51:24 +0200

    Podemos listar los paises llamando a la api de desease.sh y dejarlos fijados en el menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import {MenuItem, Button} from '@material-ui/core';
+import React, {useState, useEffect} from 'react';
+import {MenuItem, FormControl, Select} from '@material-ui/core';
 import './App.css';
 
 function App() {
-    return ( 
-    <div className = "app">
-        <h1> Hello World! </h1>
 
-        <MenuItem></MenuItem>
+    const [countires, setCountries] = useState([]);
+    const [country, setCountry] = useState('worldwide') 
+    useEffect(() => {
+      const getCountriesData = async () => {
+        await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }
+          ));
+          setCountries(countries); 
+        })
+      }
+
+      getCountriesData();
+    }, []);
+
+
+    //OnCountryChange
+    const onCountryChange = async (event) => {
+      const countryCode = event.target.value;
+      setCountry(countryCode);
+    }
+
+    return ( 
+    <div className="app">
+
+      <div className="app__header">
+        <h1> Seguimiento Covid-19 </h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            {/*Bucle por los paises*/}
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            {countires.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+        
 
       {/*HEADER*/}
       {/*Title + select country*/}
